16b376db65962bb15f9ccf9fd0844ca449ec5835
GentaPradanaPutraAgya <92007199+GentaPradanaPutraAgya@users.noreply.github.com>
2023-04-02 23:24:31 +0700

    Delete preprocessing.py

--- minggu3/preprocessing.py
+++
@@ -1,7 +0,0 @@
-tanggal_mulai,tanggal_selesai,jam_kejadian,lokasi_bencana,kelurahan,kecamatan,wilayah,jenis_bencana,jumlah_korban_bencana,presentase_korban_bencana_yang_mendapatkan_bantuan_sosial,jenis_bantuan_yang_diberikan
-"2021-05-15,2021-05-15,23:45,Jl. Manggarai Utara X Rt. 004 / 01 ,Manggarai,Tebet,Jakarta Selatan,KEBAKARAN,2 KK 5 JIWA,100,"" Natura, sandang dan logistik. """
-"2021-05-17,2021-05-17,5:30,""Jl. Taman Harapan Rt. 04,07,15 / 03 "",Cawang,Kramat Jati,Jakarta Timur,BANJIR,60 KK 300 JIWA,100,"" Makanan siap saji, Natura, sandang dan logistik. """
-"2021-05-17,2021-05-17,5:30,Kelurahan Balekambang Kecamatan Kecamatan Kramat Jati Jakarta Timur,Balekambang,Kramat Jati,Jakarta Timur,BANJIR,40 KK 200 JIWA,100,"" Makanan siap saji, Natura, sandang dan logistik. """
-"2021-05-22,2021-05-22,15:14,Jl. Samudra V Rt. 005 / 14 ,Tanjung Priok,Tanjung Priok,Jakarta Utara,KEBAKARAN,13 KK 36 JIWA,100,"" Natura, sandang dan logistik. """
-"2021-05-28,2021-05-28,12:45,Jl. Kebon Bawang 9 Rt. 015 / 01 ,Kebon Bawang,Tanjung Priok,Jakarta Utara,KEBAKARAN,2 KK 8 JIWA,100,"" Natura, sandang dan logistik. """
-"2021-05-30,2021-05-30,17:15,Jl. Angke Indah Gg.3 Rt. 001 / 02 ,Angke,Tambora,Jakarta Barat,KEBAKARAN,1 KK 4 JIWA,100,"" Natura, sandang dan logistik. """
